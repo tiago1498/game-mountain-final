bda1225673bbd14d944c8c8a90d5f85c00ba58e8
Tiago Henrique <tiagos1628@gmail.com>
2025-08-04 18:59:12 -0300

    insert asset folder

--- code/Menu.py
+++ code/Menu.py
@@ -3,6 +3,8 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
+from code.const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+
 
 class Menu:
     def __init__(self,window):
@@ -13,10 +15,15 @@ class Menu:
     def run(self, ):
         pygame.mixer.music.load('./asset/Menu.mp3')
         pygame.mixer.music.play(-1)  # -1 para continuar tocando
-
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size= 50, text="Mountain",text_color=COLOR_ORANGE,text_center_pos=((WIN_WIDTH / 2), 70))
+            self.menu_text(text_size= 50, text="Shooter", text_color=COLOR_ORANGE,text_center_pos=((WIN_WIDTH / 2), 120))
+
+            for i in range (len(MENU_OPTION)):
+                self.menu_text(text_size=20, text=MENU_OPTION[i], text_color=COLOR_WHITE, text_center_pos=((WIN_WIDTH / 2),200 +25* i ))
             pygame.display.flip()
+
 
             # checando todos os eventos
             for event in pygame.event.get():
@@ -26,7 +33,7 @@ class Menu:
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, antialias=True, color=text_color).convert_alpha()
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
 
